985d453c5c01b815607b8522a549da6bd85d8e15
Денис Коротков <Dan-Carter@mail.ru>
2023-03-12 13:33:31 +0400

    Add project files.

---
+++ Generator/Generator_Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+using std::cout;
+void GenFn(int arr[], const int n);
+void Print(int arr[], const int n);
+
+int const n = 5;
+int arr[n];
+
+void main() 
+{
+	setlocale(LC_ALL, "");
+	cout << "—генерированные числа \"5 из 36\"" << endl << endl;
+	GenFn(arr, n);
+	Print(arr, n);
+}
+
+void GenFn(int arr[], const int n) 
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 36;
+}
+}
+
+void Print(int arr[], const int n) 
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	
+}
